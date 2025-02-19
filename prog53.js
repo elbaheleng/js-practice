30e99e2b1a2990e7321ba01f13e18e680c63a060
elbaheleng <elbaheleng@gmail.com>
2025-02-19 14:20:04 +0530

    array function splice() examples

--- prog53.js
+++ prog53.js
@@ -3,3 +3,15 @@ a = [1,2,3,4,5,6]
 console.log(a.some((num) => num % 2 == 0));
 console.log(a.filter((num) => num % 2 == 0));
 console.log(a.find((num) => num % 2 == 0));
+
+
+//splice
+colours = ['red', 'orange', 'blue', 'green']
+console.log(colours);
+colours.splice(2,1) // remove blue
+console.log(colours);
+colours.splice(-2,1) // remove orange
+console.log(colours);
+colours.splice(1,1, 'purple')
+console.log(colours); // delete green and add purple
+
