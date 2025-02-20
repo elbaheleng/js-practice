484e253d41fc34dc8f67e57856fb52712e1764d2
elbaheleng <elbaheleng@gmail.com>
2025-02-20 14:47:07 +0530

    string functions startsWith, endsWith, toUpperCase, toLowerCase

---
+++ prog56.js
@@ -0,0 +1,11 @@
+//string methods
+str = 'Luminar'
+console.log(str.startsWith('L'));//true
+console.log(str.startsWith('l'));//false
+console.log(str.startsWith('Lu'));//true
+console.log(str.endsWith('nar'));//true
+console.log(str.endsWith('naR'));//false
+
+console.log(str.toUpperCase());//LUMINAR
+str2 = 'TECHNOLAB'
+console.log(str2.toLowerCase());//technolab
